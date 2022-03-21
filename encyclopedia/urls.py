b807d54ab8b01843eb43dccdd2f4d7413932e5eb
Trenton Dunstan <trentondunstan@hotmail.com>
2022-03-21 20:26:29 +1000

    Adding search functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from . import views
-
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:article>", views.article, name="article")
+    path("wiki/search", views.search, name="search"),
+    path("wiki/<str:article>", views.article, name="article"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from . import util
 
+class NewSearchForm(forms.Form):
+    search = forms.CharField(label = "Search")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": NewSearchForm()
     })
 
 def article(request, article):
@@ -17,5 +22,26 @@ def article(request, article):
     else:
         return render (request, "encyclopedia/content.html", {
         "title": article,
-        "content": util.get_entry(article)
+        "content": util.get_entry(article),
+        "form": NewSearchForm()
     })
+
+def search(request):
+    if request.method == "POST":
+        search = NewSearchForm(request.POST)
+        if search.is_valid():
+            search = search.cleaned_data["search"]
+        if not util.get_entry(search):
+            allResults = util.list_entries()
+            searchResults = []
+            for result in allResults:
+                tempSearch = search.lower()
+                tempResult = result.lower()
+                if tempSearch in tempResult:
+                    searchResults.append(result)
+            return render (request, "encyclopedia/search.html", {
+                "entries": searchResults,
+                "form": NewSearchForm()
+        })
+        else:
+            return HttpResponseRedirect(reverse("encyclopedia:article", args=[search]))
